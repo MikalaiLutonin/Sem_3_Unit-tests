7bda3e9f03637f61a701362be491541c8055bee4
Mikalai Lutonin <m.lutonin@gmail.com>
2024-02-11 21:03:33 +0300

    Sem_3_Unit-tests

---
+++ TestsMainHW.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestsMainHW {
+
+    //тесты для метода evenOddNumber
+    @Test
+    void checkEvenNumber() {
+        assertTrue(Main.evenOddNumber(100));
+    }
+    @Test
+    void checkOddNumber() {
+        assertFalse(Main.evenOddNumber(77));
+    }
+
+    //тесты для метода numberInInterval
+    @Test
+    void checkInInterval() {
+        assertTrue(Main.numberInInterval(26));
+        assertTrue(Main.numberInInterval(99));
+    }
+    @Test
+    void checkNotInInterval() {
+        assertFalse(Main.numberInInterval(25));
+        assertFalse(Main.numberInInterval(100));
+        assertFalse(Main.numberInInterval(0));
+    }
+}
